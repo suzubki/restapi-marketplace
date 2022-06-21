e06a4cf66e0d3c52b3c237cff18a9d8adc78ae5b
Darwin_N19 <darwinccn25@gmail.com>
2022-06-20 21:06:26 -0500

    middlewares added and user & admin routes

--- server/api.ts
+++ server/api.ts
@@ -1,6 +1,22 @@
 import express from "express";
+import cors from "cors";
+import helmet from "helmet";
+
+import { userRoutes, adminRoutes } from "./routes";
 
 const api = express();
+
+// if send body json
 api.use(express.json());
+
+// if send body urlencoded
+api.use(cors());
+
+// some security
+api.use(helmet());
+
+// routes
+api.use("/register/user", userRoutes);
+api.use("/register/admin", adminRoutes);
 
 export default api;
