8a954585eb377e7ba92007cf36adbbc870abee35
Darwin_N19 <darwinccn25@gmail.com>
2022-06-23 18:50:58 -0500

    api updated with the other routes!

--- server/api.ts
+++ server/api.ts
@@ -1,13 +1,28 @@
 import express from "express";
 import cors from "cors";
 import helmet from "helmet";
+import multer from "multer";
+import cookieParser from "cookie-parser";
 
-import { userRoutes, productsRoutes, fakeRoutes } from "./routes";
+import {
+    userRoutes,
+    productsRoutes,
+    couponsRoutes,
+    ordersRoutes,
+    fakeRoutes,
+} from "./routes";
 
 const api = express();
+const upload = multer();
 
 // if send body json
 api.use(express.json());
+
+// if send form-data
+api.use(upload.none());
+
+// parse cookies
+api.use(cookieParser());
 
 // cors middleware
 api.use(cors());
@@ -16,8 +31,10 @@ api.use(cors());
 api.use(helmet());
 
 // routes
-api.use("/register/user", userRoutes);
+api.use("/user", userRoutes);
 api.use("/products", productsRoutes);
+api.use("/coupons", couponsRoutes);
+api.use("/orders", ordersRoutes);
 
 // seed fake data
 api.use("/seed", fakeRoutes);
