bc4cfd28c6a7d48c046381b73af76cf2bd893b6d
Darwin_N19 <darwinccn25@gmail.com>
2022-06-23 18:50:40 -0500

    feat: seed data added

--- server/routes/fake.ts
+++ server/routes/fake.ts
@@ -1,10 +1,10 @@
 import { Router } from "express";
 import { seedData } from "../database";
-import { User } from "../models";
+import { Coupon, Product, User } from "../models";
 
 const router = Router();
 
-router.get("/", async (req, res) => {
+router.get("/user", async (req, res) => {
     try {
         // Limpiamos la data de user
         User.deleteMany();
@@ -14,6 +14,46 @@ router.get("/", async (req, res) => {
 
         return res.json({
             newUsers,
+        });
+    } catch (error) {
+        return res.json({
+            error,
+        });
+    }
+});
+
+router.get("/products", async (req, res) => {
+    try {
+        // Limpiamos la data de user
+        Product.deleteMany();
+
+        // Rellenamos la data de user
+        const newProducts = await Product.insertMany(
+            seedData.initialData.products
+        );
+
+        return res.json({
+            newProducts,
+        });
+    } catch (error) {
+        return res.json({
+            error,
+        });
+    }
+});
+
+router.get("/coupons", async (req, res) => {
+    try {
+        // Limpiamos la data de la colección coupon
+        Coupon.deleteMany();
+
+        // Rellenamos la data de la colección coupons
+        const newCoupons = await Coupon.insertMany(
+            seedData.initialData.coupons
+        );
+
+        return res.json({
+            newCoupons,
         });
     } catch (error) {
         return res.json({
